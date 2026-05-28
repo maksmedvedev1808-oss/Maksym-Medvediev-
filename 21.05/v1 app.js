0191025c9e00d22daff5071e540b696001df6df2
maksmedvedev1808-oss <maksmedvedev1808@gmail.com>
2026-05-28 14:20:59 +0300

    Add files via upload

---
+++ 21.05/v1 app.js
@@ -0,0 +1,35 @@
+import Header from './components/Header';
+import ProductCard from './components/ProductCard';
+import Footer from './components/Footer';
+
+function App() {
+    return (
+        <div>
+
+            <Header />
+
+            <ProductCard
+                title="Ноутбук"
+                price="35000"
+                category="Техніка"
+            />
+
+            <ProductCard
+                title="Смартфон"
+                price="22000"
+                category="Електроніка"
+            />
+
+            <ProductCard
+                title="Навушники"
+                price="4000"
+                category="Аксесуари"
+            />
+
+            <Footer />
+
+        </div>
+    );
+}
+
+export default App;
